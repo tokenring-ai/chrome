9ccfde5a3cc615d919425229927e8ec3c9e8787f
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:40 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,31 @@
+import TokenRingApp from "@tokenring-ai/app";
+import {ChatService} from "@tokenring-ai/chat";
+import {TokenRingPlugin} from "@tokenring-ai/app";
+import {WebSearchConfigSchema, WebSearchService} from "@tokenring-ai/websearch";
+import ChromeWebSearchProvider, {ChromeWebSearchOptionsSchema} from "./ChromeWebSearchProvider.js";
+import packageJSON from './package.json' with {type: 'json'};
+import tools from "./tools.ts";
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    app.waitForService(ChatService, chatService =>
+      chatService.addTools(packageJSON.name, tools)
+    );
+    const websearchConfig = app.getConfigSlice("websearch", WebSearchConfigSchema);
+
+    if (websearchConfig) {
+      app.waitForService(WebSearchService, cdnService => {
+        for (const name in websearchConfig.providers) {
+          const provider = websearchConfig.providers[name];
+          if (provider.type === "chrome") {
+            cdnService.registerProvider(name, new ChromeWebSearchProvider(ChromeWebSearchOptionsSchema.parse(provider)));
+          }
+        }
+      });
+    }
+  }
+} satisfies TokenRingPlugin;
