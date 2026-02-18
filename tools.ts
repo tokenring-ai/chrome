4524379d2743ed62d1372ce5e590d02fd6e6c20f
Mark Dierolf <mdierolf@cloudquote.io>
2026-02-17 21:05:55 -0800

    Update dependencies to latest

--- tools.ts
+++ tools.ts
@@ -1,10 +1,11 @@
-//import runPuppeteerScript from "./tools/runPuppeteerScript.ts";
+import runPuppeteerScript from "./tools/runPuppeteerScript.ts";
 import scrapePageText from "./tools/scrapePageText.ts";
 import scrapePageMetadata from "./tools/scrapePageMetadata.ts";
 import takeScreenshot from "./tools/takeScreenshot.ts";
 
 export default {
+  runPuppeteerScript,
   scrapePageText,
   scrapePageMetadata,
-  takeScreenshot
+  takeScreenshot,
 };
