5a7a4178a40eadb9fcd933a3cdd1afc19122980d
Mark Dierolf <mdierolf@cloudquote.io>
2026-04-08 18:35:06 -0700

    Fixing lint issues

--- tools.ts
+++ tools.ts
@@ -1,6 +1,6 @@
 import runPuppeteerScript from "./tools/runPuppeteerScript.ts";
-import scrapePageText from "./tools/scrapePageText.ts";
 import scrapePageMetadata from "./tools/scrapePageMetadata.ts";
+import scrapePageText from "./tools/scrapePageText.ts";
 import takeScreenshot from "./tools/takeScreenshot.ts";
 
 export default {
